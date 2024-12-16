b010f7f43b4ac231ee3f8b743b8f10fa6a25bc9b
Auri06 <118132700+Auri06@users.noreply.github.com>
2024-12-16 18:50:50 -0400

    Update authors.js

--- routes/authors.js
+++ routes/authors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authors = require('../data/authors');
 
-// GET /authors - List all authors
+
 router.get('/', (req, res) => {
   res.json(authors);
 });

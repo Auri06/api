a2f5e33f8d895241c7868170a8a6f9c0861a0faf
Auri06 <118132700+Auri06@users.noreply.github.com>
2024-12-16 19:09:33 -0400

    Update books.js

--- data/books.js
+++ data/books.js
@@ -1,4 +1,5 @@
 const books = [
+ 
   {
     id: 1,
     title: "1984",
@@ -8,18 +9,208 @@ const books = [
   },
   {
     id: 2,
-    title: "Animal Farm",
-    authorId: 1,
-    year: 1945,
-    available: false
-  },
-  {
-    id: 3,
-    title: "One Hundred Years of Solitude",
+    title: "Cien años de soledad",
     authorId: 2,
     year: 1967,
     available: true
+  },
+  {
+    id: 3,
+    title: "Kafka on the Shore",
+    authorId: 3,
+    year: 2002,
+    available: true
+  },
+  {
+    id: 4,
+    title: "Mrs. Dalloway",
+    authorId: 4,
+    year: 1925,
+    available: true
+  },
+  {
+    id: 5,
+    title: "Veinte poemas de amor y una canción desesperada",
+    authorId: 5,
+    year: 1924,
+    available: true
+  },
+  {
+    id: 6,
+    title: "Guerra y Paz",
+    authorId: 6,
+    year: 1869,
+    available: true
+  },
+  {
+    id: 7,
+    title: "El cuento de la criada",
+    authorId: 7,
+    year: 1985,
+    available: true
+  },
+  {
+    id: 8,
+    title: "Ficciones",
+    authorId: 8,
+    year: 1944,
+    available: true
+  },
+  {
+    id: 9,
+    title: "La metamorfosis",
+    authorId: 9,
+    year: 1915,
+    available: true
+  },
+  {
+    id: 10,
+    title: "La casa de los espíritus",
+    authorId: 10,
+    year: 1982,
+    available: true
+  },
+  {
+    id: 11,
+    title: "Things Fall Apart",
+    authorId: 11,
+    year: 1958,
+    available: true
+  },
+  {
+    id: 12,
+    title: "La insoportable levedad del ser",
+    authorId: 12,
+    year: 1984,
+    available: true
+  },
+  {
+    id: 13,
+    title: "El color púrpura",
+    authorId: 13,
+    year: 1982,
+    available: true
+  },
+  {
+    id: 14,
+    title: "Rayuela",
+    authorId: 14,
+    year: 1963,
+    available: true
+  },
+  {
+    id: 15,
+    title: "Los versos satánicos",
+    authorId: 15,
+    year: 1988,
+    available: true
+  },
+  {
+    id: 16,
+    title: "Beloved",
+    authorId: 16,
+    year: 1987,
+    available: true
+  },
+  {
+    id: 17,
+    title: "La ciudad y los perros",
+    authorId: 17,
+    year: 1963,
+    available: true
+  },
+  {
+    id: 18,
+    title: "Nunca me abandones",
+    authorId: 18,
+    year: 2005,
+    available: true
+  },
+  {
+    id: 19,
+    title: "Americanah",
+    authorId: 19,
+    year: 2013,
+    available: true
+  },
+  {
+    id: 20,
+    title: "La broma",
+    authorId: 12,
+    year: 1967,
+    available: true
+  },
+  {
+    id: 21,
+    title: "El año de la muerte de Ricardo Reis",
+    authorId: 21,
+    year: 1984,
+    available: true
+  },
+  {
+    id: 22,
+    title: "El cuaderno dorado",
+    authorId: 22,
+    year: 1962,
+    available: true
+  },
+  {
+    id: 23,
+    title: "El laberinto de la soledad",
+    authorId: 23,
+    year: 1950,
+    available: true
+  },
+  {
+    id: 24,
+    title: "Las ciudades invisibles",
+    authorId: 24,
+    year: 1972,
+    available: true
+  },
+  {
+    id: 25,
+    title: "July's People",
+    authorId: 25,
+    year: 1981,
+    available: true
+  },
+  {
+    id: 26,
+    title: "Omeros",
+    authorId: 26,
+    year: 1990,
+    available: true
+  },
+  {
+    id: 27,
+    title: "Dientes blancos",
+    authorId: 27,
+    year: 2000,
+    available: true
+  },
+  {
+    id: 28,
+    title: "Los detectives salvajes",
+    authorId: 28,
+    year: 1998,
+    available: true
+  },
+  {
+    id: 29,
+    title: "Mi nombre es Rojo",
+    authorId: 29,
+    year: 1998,
+    available: true
+  },
+  {
+    id: 30,
+    title: "Desgracia",
+    authorId: 30,
+    year: 1999,
+    available: true
   }
+
 ];
 
 module.exports = books;
